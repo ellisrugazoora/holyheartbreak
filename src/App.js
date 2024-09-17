e2dbc0fe63a240750695b30717f776e6ddf3f7c9
ellisrugazoora <ellisrgrz@gmail.com>
2024-09-16 20:50:24 -0700

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
 import Nav from "./components/Nav";
@@ -45,3 +45,59 @@ function App() {
 }
 
 export default App;
+*/
+
+import React from "react";
+import { Route, Switch, useLocation, BrowserRouter as Router } from "react-router-dom";
+import GlobalStyles from "./components/GlobalStyles";
+import Nav from "./components/Nav";
+// Import pages
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail";
+import OurWork from "./pages/OurWork";
+import Subscriptions from "./pages/Subscriptions";
+// Animation
+import { AnimatePresence } from "framer-motion";
+
+function App() {
+  return (
+    <Router basename="/holyheartbreak">
+      <div>
+        <GlobalStyles />
+        <Nav />
+        <AnimatePresence exitBeforeEnter>
+          <SwitchWithLocation />
+        </AnimatePresence>
+      </div>
+    </Router>
+  );
+}
+
+function SwitchWithLocation() {
+  const location = useLocation();
+
+  return (
+    <Switch location={location} key={location.pathname}>
+      <Route path="/" exact>
+        <AboutUs />
+      </Route>
+      <Route path="/work" exact>
+        <OurWork />
+      </Route>
+      <Route path="/contact" exact>
+        <ContactUs />
+      </Route>
+      <Route path="/subspack" exact>
+        <Subscriptions />
+      </Route>
+      <Route path="/work/:id" exact>
+        <MovieDetail />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
+
+
